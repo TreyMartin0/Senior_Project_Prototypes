5419502cf55c873800272040ea972afbdd380424
TreyMartin0 <tmarti90@calpoly.edu>
2026-04-01 15:59:39 -0700

    SIN assesment created, Hearing Test bugs fixed

---
+++ Echo/Echo/EchoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoApp.swift
+//  Echo
+//
+//  Created by Trey Martin on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct EchoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
